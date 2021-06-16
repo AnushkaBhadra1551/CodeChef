f828e9c91b30be8f769f2a4eebcf49302cb0d1da
Anushka Bhadra <anushka15official@gmail.com>
2021-06-16 23:14:56 +0530

    logical operator

---
+++ operator.py
@@ -0,0 +1,13 @@
+t = int(input())
+for i in range(0, t):
+    a, b = map(int, input().split())
+
+    if(a>b):
+        print('>')
+
+    elif(a<b):
+        print('<')
+
+    else:
+        print('=')
+    
