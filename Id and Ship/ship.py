684b976a6a2477b336b2eed9c6b622d0b5278ce8
Anushka Bhadra <anushka15official@gmail.com>
2021-07-01 20:15:21 +0530

    id

---
+++ Id and Ship/ship.py
@@ -0,0 +1,16 @@
+t = int(input())
+for i in range(0, t):
+    c = input()
+
+    if(c == 'B' or c == 'b'):
+        print("BattleShip")
+
+    elif(c == 'C' or c == 'c'):
+        print("Cruiser")
+
+    elif(c == 'D' or c == 'd'):
+        print("Destroyer")
+
+    elif(c == 'F' or c == 'f'):
+        print("Frigate")
+    
